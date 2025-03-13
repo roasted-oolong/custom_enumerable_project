8ba31184422e33667919f08428c1a59d4f6537bf
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-03-13 19:41:58 +0000

    [Complete] Array#my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,12 @@
 module Enumerable
   # Your code goes here
+  def example_method_find
+    self.each do |yes|
+      return yes if yield(yes)
+    end
+    
+    nil
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +14,9 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for item in self
+      yield(item)
+    end
+  end
 end
